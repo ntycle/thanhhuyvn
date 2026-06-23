3c686f74dee9e2ef705900cb1bd1419a2b2c5339
ntycle <mrnguyenthanhhuy@gmail.com>
2026-06-23 23:30:15 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { serverExternalPackages: ['firebase-admin', 'firebase-admin/app', 'firebase-admin/auth'],
+const nextConfig = {
   async headers() {
     return [
       {
