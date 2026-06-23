1ac3d3d0efe7921ed93f1939afec9d99edb4e645
ntycle <mrnguyenthanhhuy@gmail.com>
2026-06-23 23:18:59 +0700

    Update route.js

--- app/api/zalo/token/route.js
+++ app/api/zalo/token/route.js
@@ -2,17 +2,17 @@ import { getApps, initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { NextResponse } from 'next/server';
 
-if (!getApps().length) {
-  initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  });
-}
-
 export async function POST(req) {
+  if (!getApps().length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+
   try {
     const body = await req.json();
 
