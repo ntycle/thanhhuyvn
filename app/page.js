0e18abfe8ecd3426f9ff278dcbf707c848f25ceb
ntycle <mrnguyenthanhhuy@gmail.com>
2026-06-29 11:12:13 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -172,15 +172,14 @@ export default function Page() {
       if (isAndroid) {
         // Intent URL mở Chrome trên Android
         var intentUrl = "intent://" + url.replace(new RegExp("^https?://"), "") + "#Intent;scheme=https;package=com.android.chrome;end";
-        chromewrap.innerHTML = '<a class="fb-btn btn-chrome" href="' + intentUrl + '">🌐 Mở bằng Chrome</a>';
-        stepsEl.innerHTML = '<b>Hoặc làm thủ công:</b><br>1. Bấm vào <b>⋮</b> (góc trên phải)<br>2. Chọn <b>"Open in browser"</b>';
+        chromewrap.innerHTML = '<a class="fb-btn btn-chrome" href="' + intentUrl + '">🌐 Mở bằng Chrome<\/a>';
+        stepsEl.innerHTML = '<b>Hoặc làm thủ công:<\/b><br>1. Bấm vào <b>⋮<\/b> (góc trên phải)<br>2. Chọn <b>"Open in browser"<\/b>';
       } else if (isIOS) {
-        // iOS không có cách tự động, hướng dẫn thủ công
         chromewrap.innerHTML = '';
-        stepsEl.innerHTML = '<b>Cách mở bằng Safari:</b><br>1. Bấm vào <b>···</b> (góc dưới phải)<br>2. Chọn <b>"Open in external browser"</b><br><br><b>Cách mở bằng Chrome:</b><br>1. Bấm nút <b>Copy link</b> bên dưới<br>2. Mở Chrome → dán vào thanh địa chỉ';
+        stepsEl.innerHTML = '<b>Cách mở bằng Safari:<\/b><br>1. Bấm vào <b>···<\/b> (góc dưới phải)<br>2. Chọn <b>"Open in external browser"<\/b><br><br><b>Cách mở bằng Chrome:<\/b><br>1. Bấm nút <b>Copy link<\/b> bên dưới<br>2. Mở Chrome → dán vào thanh địa chỉ';
       } else {
         chromewrap.innerHTML = '';
-        stepsEl.innerHTML = '<b>Hướng dẫn:</b><br>1. Bấm vào <b>⋮</b> hoặc <b>···</b><br>2. Chọn <b>"Open in browser"</b> hoặc <b>"Mở bằng trình duyệt"</b>';
+        stepsEl.innerHTML = '<b>Hướng dẫn:<\/b><br>1. Bấm vào <b>⋮<\/b> hoặc <b>···<\/b><br>2. Chọn <b>"Open in browser"<\/b> hoặc <b>"Mở bằng trình duyệt"<\/b>';
       }
     })();
   `}} />
